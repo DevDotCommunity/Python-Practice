6707579be0dd186f3ad0860ef7e690a83fe765a1
piyush182004 <146834305+piyush182004@users.noreply.github.com>
2023-11-06 21:58:11 +0530

    Create question_3.py

---
+++ PIYUSH/Week_1/question_3.py
@@ -0,0 +1,10 @@
+# Prompt the user to enter two numbers
+num = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+num = num + num2
+num2 = num - num2
+num = num - num2
+print("After swapping, the first number is:", num1)
+print("After swapping, the second number is:", num2)
+#end of task3
+#using int in 3rd question
