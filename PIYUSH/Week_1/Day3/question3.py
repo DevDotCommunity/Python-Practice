986ffdf146a0ec069eaacf35427b49b45e469a33
piyush182004 <146834305+piyush182004@users.noreply.github.com>
2023-11-09 19:24:25 +0530

    Create question3.py

---
+++ PIYUSH/Week_1/Day3/question3.py
@@ -0,0 +1,4 @@
+decimal_number = int(input("Enter a decimal number: "))
+binary_number = bin(decimal_number)
+print(f"The binary representation of {decimal_number} is {binary_number[2:]}")
+#end of task3 day3
