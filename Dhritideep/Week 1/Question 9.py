6b8fe98fb6e5e8fe4e44b42a51896d0d071e0d7c
Dhritideep Saha <itsdefgh@gmail.com>
2023-11-11 21:44:42 +0530

    ninth program added

---
+++ Dhritideep/Week 1/Question 9.py
@@ -0,0 +1,6 @@
+fahrenheit = float(input("Enter temperature in fahrenheit: "))
+
+celsius = (fahrenheit - 32)/1.8
+
+print(str(fahrenheit )+ " degree Fahrenheit is equal\
+to " + str(celsius ) + " degree Celsius." )
