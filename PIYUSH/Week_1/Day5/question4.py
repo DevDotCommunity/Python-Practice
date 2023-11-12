688faa2883fd2871bc3e7d741985f4b523e49afe
piyush182004 <146834305+piyush182004@users.noreply.github.com>
2023-11-12 16:08:47 +0530

    Create question4.py

---
+++ PIYUSH/Week_1/Day5/question4.py
@@ -0,0 +1,6 @@
+def reverse_array(arr):
+    return arr[::-1]
+input_list = [10, 20, 30, 40, 50]
+reversed_list = reverse_array(input_list)
+print(f"Original List: {input_list}")
+print(f"Reversed List: {reversed_list}")
