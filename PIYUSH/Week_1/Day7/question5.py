aaf828a3cec4acd8ed3ffdd516a64b4198486b4a
piyush182004 <146834305+piyush182004@users.noreply.github.com>
2023-11-14 19:50:16 +0530

    Create question5.py

---
+++ PIYUSH/Week_1/Day7/question5.py
@@ -0,0 +1,8 @@
+# Accept a decimal number from the user
+decimal_number = int(input("Enter a decimal number: "))
+
+# Convert the decimal number to hexadecimal
+hexadecimal_number = hex(decimal_number)
+
+# Print the result
+print(f"The hexadecimal representation of {decimal_number} is: {hexadecimal_number}")
