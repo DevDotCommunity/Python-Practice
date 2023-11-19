8eeeb56a689a81851909b5f3c2627aac0067f8ac
piyush182004 <146834305+piyush182004@users.noreply.github.com>
2023-11-19 22:48:22 +0530

    Create question26.py

---
+++ PIYUSH/Week_2/Day6/question26.py
@@ -0,0 +1,6 @@
+sentence = input("Enter a sentence: ")
+words = sentence.split()
+reversed = words[::-1]
+reversed = ' '.join(reversed_words)
+print("Reversed sentence:", reversed)
+//end of program 26
